09f57b91f2bce2b4844e395ae8aefa85acd592c1
Efobi-Francis <francisefobi@gmail.com>
2022-10-14 06:02:02 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main: The main function indicates the entry point of the program
+ *
+ * Return: The return keyword should return 0 in this case
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
