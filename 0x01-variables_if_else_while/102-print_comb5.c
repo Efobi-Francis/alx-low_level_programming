2fc5108c745bed13458594cfadbffe5a26f38b8a
Efobi-Francis <francisefobi@gmail.com>
2022-10-15 14:09:24 -0700

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,55 +1,44 @@
 #include <stdio.h>
-#include <stdlib.h>
-
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
-{
-	int i;
-	int j;
-	int k;
-	int l;
 
-	i = 48;
-	while (i <= 57)
-	{
-		j = 48;
-		while (j <= 57)
-		{
-			k = 48;
-			while (k <= 57)
-			{
-				if (k == 48)
-					l = 49;
-				else
-					l = 48;
-				while (l <= 57)
-				{
-					putchar(i);
-					putchar(j);
-					putchar(32);
-					putchar(k);
-					putchar(l);
-					if (i == 57 && j == 56 && k == 57 && l == 57)
-					{
-						putchar('\n');
-						exit(0);
-					}
-					else
-					{
-						putchar(44);
-						putchar(32);
-						l++;
-					}
-				}
-				k++;
-			}
-			j++;
-		}
-		i++;
-	}
-	return (0);
+{
+int first_nums; /*Decarling statement*/
+int second_nums;
+
+/*for 0-9 with ASCII*/
+for (first_nums = 0; first_nums < 100 ; first_nums++)
+{
+for (second_nums = first_nums + 1 ; second_nums < 100; second_nums++)
+{
+
+putchar (first_nums / 10 + '0'); /*print  first nums, first digit*/
+putchar (first_nums % 10 + '0'); /*print  first nums, second digit*/
+
+putchar (' '); /*prirint a space*/
+
+putchar (second_nums / 10 + '0'); /*print  sencond nums, first digit*/
+putchar (second_nums % 10 + '0'); /*print  second nums, second digit*/
+
+/*break program if the condition is true (if is 98 99)*/
+if (first_nums == 98 && second_nums == 99)
+{
+break;
+}
+
+putchar(','); /*print (,)*/
+putchar (' '); /*prirint a space*/
+
+} /*end for #2*/
+
+} /*end for #1*/
+
+putchar('\n'); /*new line*/
+
+return (0);
+
 }
